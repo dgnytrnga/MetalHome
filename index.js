093b01fe741c39fe75cb1463539ed4bd3124c1f0
dgnytrnga <dgnytrnga@gmail.com>
2023-09-08 13:19:32 +0300

    Edited and commited

--- index.js
+++ index.js
@@ -28,3 +28,28 @@
 
 // // Sayfa yüklendiğinde carousel'ı başlatma
 // updateCarousel();
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  var currentIndex = 0;
+  var items = document.querySelectorAll(".carousel-item");
+  
+  function showItem(index) {
+    items.forEach(function (item) {
+      item.style.display = "none";
+    });
+    items[index].style.display = "block";
+  }
+  
+  showItem(currentIndex);
+  
+  document.getElementById("prevBtn").addEventListener("click", function () {
+    currentIndex = (currentIndex - 1 + items.length) % items.length;
+    showItem(currentIndex);
+  });
+  
+  document.getElementById("nextBtn").addEventListener("click", function () {
+    currentIndex = (currentIndex + 1) % items.length;
+    showItem(currentIndex);
+  });
+});
